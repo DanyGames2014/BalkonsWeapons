a747490a89356f40d6188b672b54a63a355780ef
Ralf2oo2 <ralf2oo2@hotmail.com>
2026-06-11 23:08:35 +0200

    Legacy spear rendering

--- src/main/java/net/danygames2014/balkonsweapons/client/render/entity/SpearEntityRenderer.java
+++ src/main/java/net/danygames2014/balkonsweapons/client/render/entity/SpearEntityRenderer.java
@@ -15,6 +15,7 @@ import net.modificationstation.stationapi.api.client.render.RendererAccess;
 import net.modificationstation.stationapi.api.client.render.model.BakedModelRenderer;
 import net.modificationstation.stationapi.api.client.render.model.json.ModelTransformation;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
+import org.lwjgl.opengl.EXTRescaleNormal;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
@@ -24,7 +25,58 @@ public class SpearEntityRenderer extends EntityRenderer {
         SpearEntity spear = (SpearEntity) entity;
         float tickDelta = Minecraft.INSTANCE.timer.tickDelta;
         if(false) {
-
+            bindTexture("/assets/balkonsweapons/stationapi/textures/entity/spear.png");
+            GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+            GL11.glPushMatrix();
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glTranslated(x, y, z);
+            GL11.glRotatef(spear.prevYaw + (spear.yaw - spear.prevYaw) * tickDelta - 90.0f, 0.0f, 1.0f, 0.0f);
+            GL11.glRotatef(spear.prevPitch + (spear.pitch - spear.prevPitch) * tickDelta, 0.0f, 0.0f, 1.0f);
+            Tessellator tess = Tessellator.INSTANCE;
+            float[] color = spear.getMaterialColor();
+            double length = 20.0;
+            GL11.glEnable(EXTRescaleNormal.GL_RESCALE_NORMAL_EXT);
+            float f13 = spear.shake - tickDelta;
+            if (f13 > 0.0f) {
+                float f14 = -MathHelper.sin(f13 * 3.0f) * f13;
+                GL11.glRotatef(f14, 0.0f, 0.0f, 1.0f);
+            }
+            GL11.glRotatef(45.0f, 1.0f, 0.0f, 0.0f);
+            GL11.glScalef(0.05625f, 0.05625f, 0.05625f);
+            GL11.glTranslatef(-4.0f, 0.0f, 0.0f);
+            GL11.glNormal3f(0.05625f, 0.0f, 0.0f);
+            tess.startQuads();
+            tess.vertex(-length, -2.0, -2.0, 0.0, 0.15625);
+            tess.vertex(-length, -2.0, 2.0, 0.15625, 0.15625);
+            tess.vertex(-length, 2.0, 2.0, 0.15625, 0.3125);
+            tess.vertex(-length, 2.0, -2.0, 0.0, 0.3125);
+            tess.draw();
+            GL11.glNormal3f(-0.05625f, 0.0f, 0.0f);
+            tess.startQuads();
+            tess.vertex(-length, 2.0, -2.0, 0.0, 0.15625);
+            tess.vertex(-length, 2.0, 2.0, 0.15625, 0.15625);
+            tess.vertex(-length, -2.0, 2.0, 0.15625, 0.3125);
+            tess.vertex(-length, -2.0, -2.0, 0.0, 0.3125);
+            tess.draw();
+            for (int j = 0; j < 4; ++j) {
+                GL11.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+                GL11.glNormal3f(0.0f, 0.0f, 0.05625f);
+                tess.startQuads();
+                tess.color(1F, 1F, 1F);
+                tess.vertex(-length, -2.0, 0.0, 0.0, 0.0);
+                tess.vertex(length, -2.0, 0.0, 1.0, 0.0);
+                tess.vertex(length, 2.0, 0.0, 1.0, 0.15625);
+                tess.vertex(-length, 2.0, 0.0, 0.0, 0.15625);
+                tess.color(color[0], color[1], color[2]);
+                tess.vertex(-length, -2.0, 0.0, 0.0, 0.3125);
+                tess.vertex(length, -2.0, 0.0, 1.0, 0.3125);
+                tess.vertex(length, 2.0, 0.0, 1.0, 0.46875);
+                tess.vertex(-length, 2.0, 0.0, 0.0, 0.46875);
+                tess.draw();
+            }
+            GL11.glDisable(EXTRescaleNormal.GL_RESCALE_NORMAL_EXT);
+            GL11.glEnable(GL11.GL_LIGHTING);
+            GL11.glPopMatrix();
         } else {
             GL11.glPushMatrix();
             StationRenderAPI.getBakedModelManager().getAtlas(Atlases.GAME_ATLAS_TEXTURE).bindTexture();
